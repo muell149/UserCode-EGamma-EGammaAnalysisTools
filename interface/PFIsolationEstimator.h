9258bbcc40b339fd49e029aba2d1a25e8d72bf07
vchetlur <>
2012-04-02 19:29:29 +0000

    Setter functions for the isolation variable veto regions

--- interface/PFIsolationEstimator.h
+++ interface/PFIsolationEstimator.h
@@ -50,17 +50,58 @@ class PFIsolationEstimator{
     kPhoton  =  1            // MVA for triggering electrons
   };
   
-  void     initialize( );
+  void     initialize( Bool_t  bApplyVeto, Int_t iParticleType );
   
   Bool_t   isInitialized() const { return fisInitialized; }
   
 
-  Float_t fElectronIsolation(const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
-  Float_t fPhotonIsolation(const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
+  /*  Float_t fElectronIsolation(const reco::PFCandidate * pfCandidate, const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
+  Float_t fPhotonIsolation(const reco::PFCandidate * pfCandidate,const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
 
-  Float_t* fElectronIsolationInRings(const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
-  Float_t* fPhotonIsolationInRings(const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
+  Float_t* fElectronIsolationInRings(const reco::PFCandidate * pfCandidate,const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
+  Float_t* fPhotonIsolationInRings(const reco::PFCandidate * pfCandidate,const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
+  */
+
+  Float_t fGetIsolation(const reco::PFCandidate * pfCandidate,const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
+  Float_t* fGetIsolationInRings(const reco::PFCandidate * pfCandidate,const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >);
   
+ 
+  int chargedHadronVertex( const reco::VertexCollection& vertices, const reco::PFCandidate& pfcand ) const;
+
+  void setConeSize(Float_t fValue = 0.4){ fConeSize = fValue;};
+
+  //Veto booleans
+  void setApplyVeto(Bool_t bValue = kTRUE){  bApplyVeto = bValue;};
+  void setDeltaRVetoBarrel(Bool_t bValue = kTRUE){  bDeltaRVetoBarrel = bValue;};
+  void setDeltaRVetoEndcap(Bool_t bValue = kTRUE){  bDeltaRVetoEndcap = bValue;};
+  void setRectangleVetoBarrel(Bool_t bValue = kTRUE){  bRectangleVetoBarrel = bValue;};
+  void setRectangleVetoEndcap(Bool_t bValue = kTRUE){  bRectangleVetoEndcap = bValue;};
+
+  //Veto Values
+  void setDeltaRVetoBarrelPhotons(Float_t fValue = -1.){fDeltaRVetoBarrelPhotons=fValue;};
+  void setDeltaRVetoBarrelNeutrals(Float_t fValue = -1.){fDeltaRVetoBarrelNeutrals=fValue;};
+  void setDeltaRVetoBarrelCharged(Float_t fValue = -1.){fDeltaRVetoBarrelPhotons=fValue;};
+  void setDeltaRVetoEndcapPhotons(Float_t fValue = -1.){fDeltaRVetoEndcapPhotons=fValue;};
+  void setDeltaRVetoEndcapNeutrals(Float_t fValue = -1.){fDeltaRVetoEndcapNeutrals=fValue;};
+  void setDeltaRVetoEndcapCharged(Float_t fValue = -1.){fDeltaRVetoEndcapPhotons=fValue;};
+
+  
+  void setRectangleDeltaPhiVetoBarrelPhotons(Float_t fValue = -1.){fRectangleDeltaPhiVetoBarrelPhotons=fValue;};
+  void setRectangleDeltaPhiVetoBarrelNeutrals(Float_t fValue = -1.){fRectangleDeltaPhiVetoBarrelNeutrals=fValue;};
+  void setRectangleDeltaPhiVetoBarrelCharged(Float_t fValue = -1.){fRectangleDeltaPhiVetoBarrelPhotons=fValue;};
+  void setRectangleDeltaPhiVetoEndcapPhotons(Float_t fValue = -1.){fRectangleDeltaPhiVetoEndcapPhotons=fValue;};
+  void setRectangleDeltaPhiVetoEndcapNeutrals(Float_t fValue = -1.){fRectangleDeltaPhiVetoEndcapNeutrals=fValue;};
+  void setRectangleDeltaPhiVetoEndcapCharged(Float_t fValue = -1.){fRectangleDeltaPhiVetoEndcapPhotons=fValue;};
+  
+
+  void setRectangleDeltaEtaVetoBarrelPhotons(Float_t fValue = -1.){fRectangleDeltaEtaVetoBarrelPhotons=fValue;};
+  void setRectangleDeltaEtaVetoBarrelNeutrals(Float_t fValue = -1.){fRectangleDeltaEtaVetoBarrelNeutrals=fValue;};
+  void setRectangleDeltaEtaVetoBarrelCharged(Float_t fValue = -1.){fRectangleDeltaEtaVetoBarrelPhotons=fValue;};
+  void setRectangleDeltaEtaVetoEndcapPhotons(Float_t fValue = -1.){fRectangleDeltaEtaVetoEndcapPhotons=fValue;};
+  void setRectangleDeltaEtaVetoEndcapNeutrals(Float_t fValue = -1.){fRectangleDeltaEtaVetoEndcapNeutrals=fValue;};
+  void setRectangleDeltaEtaVetoEndcapCharged(Float_t fValue = -1.){fRectangleDeltaEtaVetoEndcapPhotons=fValue;};
+
+
 
 #ifndef STANDALONE
  
@@ -69,10 +110,52 @@ class PFIsolationEstimator{
  
  private:
  
+
+  Int_t                     iParticleType;
+
   Bool_t                    fisInitialized;
   Float_t                   fIsolation;
+  Float_t                   fIsolationPhoton;
+  Float_t                   fIsolationNeutral;
+  Float_t                   fIsolationCharged;
+  Float_t                   fIsolationChargedAll;
+  
   Float_t                   fIsolationInRings[10];
   
+  
+  Bool_t                    checkClosestZVertex;
+  Float_t                   fConeSize;
+  Bool_t                    bApplyVeto;
+  
+  Bool_t                    bDeltaRVetoBarrel; 
+  Bool_t                    bDeltaRVetoEndcap; 
+  
+  Bool_t                    bRectangleVetoBarrel; 
+  Bool_t                    bRectangleVetoEndcap; 
+  
+  Float_t                   fDeltaRVetoBarrelPhotons; 
+  Float_t                   fDeltaRVetoBarrelNeutrals;
+  Float_t                   fDeltaRVetoBarrelCharged;
+
+  Float_t                   fDeltaRVetoEndcapPhotons; 
+  Float_t                   fDeltaRVetoEndcapNeutrals;
+  Float_t                   fDeltaRVetoEndcapCharged;  
+
+  Float_t                   fRectangleDeltaPhiVetoBarrelPhotons; 
+  Float_t                   fRectangleDeltaPhiVetoBarrelNeutrals;
+  Float_t                   fRectangleDeltaPhiVetoBarrelCharged;
+
+  Float_t                   fRectangleDeltaPhiVetoEndcapPhotons; 
+  Float_t                   fRectangleDeltaPhiVetoEndcapNeutrals;
+  Float_t                   fRectangleDeltaPhiVetoEndcapCharged;
+  
+  Float_t                   fRectangleDeltaEtaVetoBarrelPhotons; 
+  Float_t                   fRectangleDeltaEtaVetoBarrelNeutrals;
+  Float_t                   fRectangleDeltaEtaVetoBarrelCharged;
+
+  Float_t                   fRectangleDeltaEtaVetoEndcapPhotons; 
+  Float_t                   fRectangleDeltaEtaVetoEndcapNeutrals;
+  Float_t                   fRectangleDeltaEtaVetoEndcapCharged;
 
 };
 
--- src/PFIsolationEstimator.cc
+++ src/PFIsolationEstimator.cc
@@ -39,35 +39,183 @@ PFIsolationEstimator::~PFIsolationEstimator()
 }
 
 //--------------------------------------------------------------------------------------------------
-void PFIsolationEstimator::initialize( ) {
+void PFIsolationEstimator::initialize( Bool_t  bApplyVeto, Int_t iParticleType ) {
+
+
+  //By default check for an option vertex association
+  checkClosestZVertex = kTRUE;
+
+  //Default cone size is 0.4 for electrons and photons
+  setConeSize(0.4);
   
+  //Apply vetoes
+  setApplyVeto(bApplyVeto);
+  
+  setDeltaRVetoBarrelPhotons();
+  setDeltaRVetoBarrelNeutrals();
+  setDeltaRVetoBarrelCharged();
+  setDeltaRVetoEndcapPhotons();
+  setDeltaRVetoEndcapNeutrals();
+  setDeltaRVetoEndcapCharged();
+
+  
+  setRectangleDeltaPhiVetoBarrelPhotons();
+  setRectangleDeltaPhiVetoBarrelNeutrals();
+  setRectangleDeltaPhiVetoBarrelCharged();
+  setRectangleDeltaPhiVetoEndcapPhotons();
+  setRectangleDeltaPhiVetoEndcapNeutrals();
+  setRectangleDeltaPhiVetoEndcapCharged();
+  
+
+  setRectangleDeltaEtaVetoBarrelPhotons();
+  setRectangleDeltaEtaVetoBarrelNeutrals();
+  setRectangleDeltaEtaVetoBarrelCharged();
+  setRectangleDeltaEtaVetoEndcapPhotons();
+  setRectangleDeltaEtaVetoEndcapNeutrals();
+  setRectangleDeltaEtaVetoEndcapCharged();
+
+
+  if(bApplyVeto && iParticleType==kElectron){
+    //Setup veto conditions for electrons
+    setDeltaRVetoBarrel(kFALSE);
+    setDeltaRVetoEndcap(kTRUE);
+    setRectangleVetoBarrel(kFALSE);
+    setRectangleVetoEndcap(kFALSE);
+    
+    //Current recommended default value for the electrons
+    setDeltaRVetoEndcapPhotons(0.08);
+    setDeltaRVetoEndcapCharged(0.015);
+
+
+  }else{
+    //Setup veto conditions for photons
+    setDeltaRVetoBarrel(kTRUE);
+    setDeltaRVetoEndcap(kTRUE);
+    setRectangleVetoBarrel(kTRUE);
+    setRectangleVetoEndcap(kTRUE);
+  }
+  
+
   fisInitialized = kTRUE;
 
 }
 
-
+/*
 
 //--------------------------------------------------------------------------------------------------
-Float_t PFIsolationEstimator::fElectronIsolation(const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection > ) {
+Float_t PFIsolationEstimator::fElectronIsolation(const reco::PFCandidate * pfCandidate,const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection > ) 
+{
+  
+  fIsolation=0.0;
+
+  
   
   return fIsolation;
 }
  
 //--------------------------------------------------------------------------------------------------
-Float_t PFIsolationEstimator::fPhotonIsolation( const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
+Float_t PFIsolationEstimator::fPhotonIsolation(const reco::PFCandidate * pfCandidate, const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
   
   return fIsolation;
 }
 
 
 //--------------------------------------------------------------------------------------------------
-Float_t* PFIsolationEstimator::fElectronIsolationInRings( const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
+Float_t* PFIsolationEstimator::fElectronIsolationInRings(const reco::PFCandidate * pfCandidate, const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
   
   return fIsolationInRings;
 }
  
 //--------------------------------------------------------------------------------------------------
-Float_t* PFIsolationEstimator::fPhotonIsolationInRings( const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
+Float_t* PFIsolationEstimator::fPhotonIsolationInRings(const reco::PFCandidate * pfCandidate, const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
   
   return fIsolationInRings;
 }
+*/
+
+ 
+//--------------------------------------------------------------------------------------------------
+Float_t PFIsolationEstimator::fGetIsolation(const reco::PFCandidate * pfCandidate, const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
+  
+  return fIsolation;
+}
+
+
+//--------------------------------------------------------------------------------------------------
+Float_t* PFIsolationEstimator::fGetIsolationInRings(const reco::PFCandidate * pfCandidate, const reco::PFCandidateCollection* pfParticlesColl, edm::Handle< reco::VertexCollection >) {
+  
+  return fIsolationInRings;
+}
+
+
+int PFIsolationEstimator::chargedHadronVertex( const reco::VertexCollection& vertices, const reco::PFCandidate& pfcand ) const {
+
+  //code copied from Florian's PFNoPU class
+
+
+  reco::TrackBaseRef trackBaseRef( pfcand.trackRef() );
+
+  size_t  iVertex = 0;
+  unsigned index=0;
+  unsigned nFoundVertex = 0;
+  typedef reco::VertexCollection::const_iterator IV;
+  typedef reco::Vertex::trackRef_iterator IT;
+  float bestweight=0;
+  for(IV iv=vertices.begin(); iv!=vertices.end(); ++iv, ++index) {
+
+    const reco::Vertex& vtx = *iv;
+
+    // loop on tracks in vertices
+    for(IT iTrack=vtx.tracks_begin();
+        iTrack!=vtx.tracks_end(); ++iTrack) {
+
+      const reco::TrackBaseRef& baseRef = *iTrack;
+
+      // one of the tracks in the vertex is the same as 
+      // the track considered in the function
+      if(baseRef == trackBaseRef ) {
+        float w = vtx.trackWeight(baseRef);
+        //select the vertex for which the track has the highest weight
+        if (w > bestweight){
+          bestweight=w;
+          iVertex=index;
+          nFoundVertex++;
+        }
+      }
+    }
+  }
+
+  if (nFoundVertex>0){
+    if (nFoundVertex!=1)
+      edm::LogWarning("TrackOnTwoVertex")<<"a track is shared by at least two verteces. Used to be an assert";
+    return iVertex;
+  }
+  // no vertex found with this track. 
+
+  // optional: as a secondary solution, associate the closest vertex in z
+  if ( checkClosestZVertex ) {
+
+    double dzmin = 10000;
+    double ztrack = pfcand.vertex().z();
+    bool foundVertex = false;
+    index = 0;
+    for(IV iv=vertices.begin(); iv!=vertices.end(); ++iv, ++index) {
+
+      double dz = fabs(ztrack - iv->z());
+      if(dz<dzmin) {
+        dzmin = dz;
+        iVertex = index;
+        foundVertex = true;
+      }
+    }
+
+    if( foundVertex ) 
+      return iVertex;  
+  
+  }
+
+
+  return -1 ;
+}
+
+
